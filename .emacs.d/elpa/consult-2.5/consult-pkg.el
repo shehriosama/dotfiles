6155bfeca533293f49be53e5dd5cc62d81951d39
shehriosama <ashehrigit19@gmail.com>
2025-07-01 01:55:50 +0300

    Delete unwanted files

--- .emacs.d/elpa/consult-2.5/consult-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from consult.el  -*- mode: lisp-data; no-byte-compile: t -*-
-(define-package "consult" "2.5" "Consulting completing-read" '((emacs "28.1") (compat "30")) :commit "b6fd7feda74267a53f9f1cf1bd122e519a308132" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
