47b3df169bfe9aa1e7fc5e1af8cdee3ba57510f6
shehriosama <ashehrigit19@gmail.com>
2025-07-10 09:12:15 +0300

    Update dotfiles

--- .emacs.d/elpa/consult-2.6/consult-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from consult.el  -*- mode: lisp-data; no-byte-compile: t -*-
-(define-package "consult" "2.6" "Consulting completing-read" '((emacs "28.1") (compat "30")) :commit "4314c45d84d9e00d45d2b611562d8fe2e4ddcf48" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
